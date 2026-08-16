ff5fe815f2e3517ba7d15bbd0b8be642db7cebf6
Sarobidy <hei.sarbidy.8@gmail.com>
2026-08-16 18:51:49 +0300

    feat: add userRepository

---
+++ src/Repository/userRepository.ts
@@ -0,0 +1,39 @@
+import { pool } from "./bd";
+import { User } from "../Model/userModel";
+
+
+export class UserRepository {
+    async findByEmail(email: string): Promise<User | null>{
+        const result = await pool.query("SELECT id, email, password_hash from users where email = $1", [email]);
+
+        if (result.rows.length===0){return null;}
+        return{
+            id: result.rows[0].id,
+            email: result.rows[0].email,
+            passwordHash: result.rows[0].password_hash
+        };
+
+    }
+
+    async findById(id: number): Promise<User | null>{
+        const result = await pool.query("SELECT id, email, password_hash from users where id = $1", [id]);
+
+        if (result.rows.length===0){return null;}
+        return{
+            id: result.rows[0].id,
+            email: result.rows[0].email,
+            passwordHash: result.rows[0].password_hash
+        };
+
+    }
+
+    async create(email: string, passwordHash: string): Promise<User>{
+        const result = await pool.query(`INSERT INTO users (email, password_hash) values ($1, $2) returning id, email, password_hash`, [email,passwordHash]);
+
+        return{
+            id: result.rows[0].id,
+            email: result.rows[0].email,
+            passwordHash: result.rows[0].password_hash
+        };
+    }
+}
